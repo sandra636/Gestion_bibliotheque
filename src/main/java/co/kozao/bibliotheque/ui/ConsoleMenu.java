27e84af50b37b9784f9ff570a63e27e5e2a0e808
IntelligCodeGirl 🩵 <angenjuikom18@gmail.com>
2026-06-12 13:47:22 +0100

    ajout de consoleMenu

--- src/main/java/co/kozao/bibliotheque/ui/ConsoleMenu.java
+++ src/main/java/co/kozao/bibliotheque/ui/ConsoleMenu.java
@@ -1,5 +1,32 @@
 package co.kozao.bibliotheque.ui;
+import java.util.Scanner;
+import co.kozao.bibliotheque.service.LibraryService;
 
 public class ConsoleMenu {
+	public static void main(String[]args) {
+    private final LibraryService service;
+    private final Scanner scanner;
 
+    public ConsoleMenu(LibraryService service) {
+        this.service = service;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        // boucle principale
+    }
+
+    private void ajouterLivre() {} //pour appeler la  
+
+    private void ajouterMembre() {}
+
+    private void emprunterLivre() {}
+
+    private void retournerLivre() {}
+
+    private void reserverLivre() {}
+
+    private void afficherLivres() {}
+
+    private void afficherMembres() {}
 }
